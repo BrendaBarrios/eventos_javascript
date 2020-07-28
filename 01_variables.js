e6227cf4c48a39c9cc9702a6d0101a393355f717
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-07-27 19:37:30 -0500

    Variables en javascript

---
+++ 01_variables.js
@@ -0,0 +1,35 @@
+/**
+ * console.log()
+ * Usado para imprimir
+ */
+console.log("Aprendiendo Javascript JS!")
+
+
+/**
+ * Pidiendo variables 
+ * Declaración de variables 
+ * var nombre_var
+ */
+
+ var nombre
+
+ /**
+  * Asignando valores
+  */
+
+  nombre ='Brenda'
+  console.log('Hello '+ nombre)
+
+  /**
+   * Declarando y asignando en una misma linea
+   */
+
+var edad = 18 , apellido = 'Barrios'
+
+console.log('Hola ' + nombre + ' '+ apellido)
+
+console.log('Tengo '+ edad + ' años');
+
+var peso = 75
+
+console.log(peso)

690d27026d2496fdeb751dc68f32af57988014ca
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-09-01 14:53:08 -0500

    subiendo ejercicio de numeros v1.0

--- 02_clase_luis.js
+++ 02_clase_luis.js
@@ -33,16 +33,64 @@ for (i = 0; i < nums.length; i++){
 console.log(mayor)
 
 
-// Crea una aplicación que pida un número y calcule su factorial 
+// pida un número y calcule su factorial 
 //(El factorial de un número es el producto de todos los enteros entre 1 y el propio número y se representa 
 //por el número seguido de un signo de exclamación. Por ejemplo 5! = 1x2x3x4x5=120),
 
+
 function factorial(num){
-    let resultado = 0;
-    for (i=1; i <= num; i++){
-        resultado += (num -1)*i
+    let resultado = 1;
+    if (num == 0){
+      return 1
     }
+    for (i=1; i <= num; i++){
+      resultado = resultado *i
+    }
+
     return resultado
+    
 }
 
-factorial(5)
+console.log(factorial(5))
+
+//Algoritmo que pida números hasta que se introduzca un cero. 
+//Debe imprimir la suma y la media de todos los números introducidos.
+let suma = 0
+let media = 0
+let numero = 0
+
+numero = prompt("Ingresa un numero")
+
+if (numero == 0){
+  console.log("SALISTE DEL PROGRAMA") 
+}
+
+  do{
+    numero = prompt("Ingresa un numero")
+    suma += numero
+  }while (numero > 0)
+
+media = suma / numero
+
+console.log(suma)
+console.log(media)
+
+
+ // do{
+   // suma += i
+   // console.log("resultado" + suma)
+  //} while (numero > 0)
+ 
+
+
+
+
+
+
+//Realizar un algoritmo que pida números 
+//(se pedirá por teclado la cantidad de números a introducir). 
+//El programa debe informar de cuantos números introducidos son mayores que 0, menores que 0 e iguales a 0.
+
+//Algoritmo que pida caracteres e imprima ‘VOCAL’ si son vocales y ‘NO VOCAL’ en caso contrario, el programa termina cuando se introduce un espacio.
+
+//Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
